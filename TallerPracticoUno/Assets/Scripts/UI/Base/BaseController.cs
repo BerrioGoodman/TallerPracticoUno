e902b79c93a3d506b3f6b5807f776b205eed4973
Pelimorita <mariana.araquer@hotmail.com>
2025-06-20 19:26:57 -0500

    Assets

--- TallerPracticoUno/Assets/Scripts/UI/Base/BaseController.cs
+++ TallerPracticoUno/Assets/Scripts/UI/Base/BaseController.cs
@@ -6,6 +6,7 @@ public abstract class BaseController : MonoBehaviour
 {
     protected CanvasGroup mainCanvasGroup;
 
+
     protected virtual void Awake()
     {
         mainCanvasGroup = GetComponent<CanvasGroup>();
